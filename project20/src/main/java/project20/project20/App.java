9cd05ce5f74e3c2c805f104a28440ac9b0faa545
naren905 <narendranese10@gmail.com>
2022-05-10 18:10:53 +0530

    first commit

---
+++ project20/src/main/java/project20/project20/App.java
@@ -0,0 +1,13 @@
+package project20.project20;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ project20/src/test/java/project20/project20/Test.java
@@ -0,0 +1,11 @@
+package project20.project20;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		System.out.println("started successfully");
+
+	}
+
+}
